93ec24733caf9d9618569321f87687b420f7b9ed
Máté Bartók <matebartok@Mates-Mac-mini.local>
2024-05-27 21:30:13 +0200

    Styling NextJs Project

--- components/main-header.js
+++ components/main-header.js
@@ -1,15 +1,16 @@
 import Link from "next/link";
+import classes from "./main-header.module.css"
 
 import logoImg from "@/assets/logo.png"
 
 export default function MainHeader() {
-    return <header>
-        <Link href="/">
+    return <header className={classes.header}>
+        <Link className={classes.logo} href="/">
             <img src={logoImg.src} alt="A plate with food on it" />
             NextLevel Food
         </Link>
 
-         <nav>
+         <nav className={classes.nav}>
             <ul>
                 <li>
                     <Link href="/meals">Browse Meals</Link>
