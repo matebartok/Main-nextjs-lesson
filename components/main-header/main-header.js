1f30451cf9b1769301196979df878ab230d20e0d
Máté Bartók <matebartok@Mates-Mac-mini.local>
2024-05-27 22:47:03 +0200

    Using Client Components Efficiently

--- components/main-header/main-header.js
+++ components/main-header/main-header.js
@@ -2,30 +2,31 @@ import Link from "next/link";
 import Image from "next/image";
 
 import MainHeaderBackground from "./main-header-background";
+import Navlink from "./nav-link";
 
 import classes from "./main-header.module.css";
 import logoImg from "@/assets/logo.png";
 
 export default function MainHeader() {
   return (
-  <>
-    <MainHeaderBackground/>
-    <header className={classes.header}>
-      <Link className={classes.logo} href="/">
-        <Image src={logoImg} alt="A plate with food on it" priority />
-        NextLevel Food
-      </Link>
-      <nav className={classes.nav}>
-        <ul>
-          <li>
-            <Link href="/meals">Browse Meals</Link>
-          </li>
-          <li>
-            <Link href="/meals">Foodies Community</Link>
-          </li>
-        </ul>
-      </nav>
-    </header>
-  </>
-  )
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link className={classes.logo} href="/">
+          <Image src={logoImg} alt="A plate with food on it" priority />
+          NextLevel Food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li>
+              <Navlink href="/meals">Browse Meals</Navlink>
+            </li>
+            <li>
+            <Navlink href="/community">Foodies Community</Navlink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
 }
