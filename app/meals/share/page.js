b050f53f3354488e633e1fff282a299c0fdd26ce
Máté Bartók <matebartok@Mates-Mac-mini.local>
2024-05-31 16:44:08 +0200

    Adding Server-Side Input Validation

--- app/meals/share/page.js
+++ app/meals/share/page.js
@@ -1,10 +1,14 @@
-import ImagePicker from '@/components/meals/image-picker';
-import classes from './page.module.css';
 
-import { shareMeal } from '@/lib/actions';
+import ImagePicker from "@/components/meals/image-picker";
+import classes from "./page.module.css";
+
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
+
+import { shareMeal } from "@/lib/actions";
 
 export default function ShareMealPage() {
- 
+
+
 
   return (
     <>
@@ -43,9 +47,9 @@ export default function ShareMealPage() {
               required
             ></textarea>
           </p>
-          <ImagePicker label="Your image" name="image"/>
+          <ImagePicker label="Your image" name="image" />
           <p className={classes.actions}>
-            <button type="submit">Share Meal</button>
+            <MealsFormSubmit />
           </p>
         </form>
       </main>
